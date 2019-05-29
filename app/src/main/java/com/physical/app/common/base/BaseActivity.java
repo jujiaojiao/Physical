9fb4e7257ff38a3f9184f95d7f0fec74a53ef6c5
jujiaojiao <452467173@qq.com>
2019-05-29 17:58:23 +0800

    切图

--- app/src/main/java/com/physical/app/common/base/BaseActivity.java
+++ app/src/main/java/com/physical/app/common/base/BaseActivity.java
@@ -12,6 +12,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -34,15 +35,27 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mContext=this;
+        mContext = this;
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+            window.setNavigationBarColor(Color.TRANSPARENT);
+        }
     }
 
-    public void showToast(String msg){
+    public void showToast(String msg) {
         ToastUtil.show(msg);
 //        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 
-    public void startTo(Class c){
+    public void startTo(Class c) {
         startActivity(new Intent(this, c));
     }
 
@@ -52,8 +65,8 @@ public class BaseActivity extends AppCompatActivity {
 
     public boolean userIsLogin(boolean startToLogin) {
         User user = AppData.getInstance().getUser();
-        if(null == user){
-            if(startToLogin){
+        if (null == user) {
+            if (startToLogin) {
                 startTo(LoginActivity.class);
             }
             return false;
